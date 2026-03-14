d59507faf046346e46672a198bf0fce7b6bce3af
Michel Silva <michel.gomes.silva@gmail.com>
2026-03-14 11:46:04 +0000

    feat: solution

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from practice-1!")
+print("--- Classificador de Idade ---\n")
 
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
 
-if __name__ == "__main__":
-    main()
+if idade < 18:
+    print(f"\nOlá, {nome}! Como você tem {idade} anos, você é menor de idade.")
+elif idade >= 18 and idade <= 65:
+    print(f"\nOlá, {nome}! Como você tem {idade} anos, você é um adulto.")
+else:
+    print(f"\nOlá, {nome}! Como você tem {idade} anos, você é uma pessoa que se aposentou.")
